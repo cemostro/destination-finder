974375016b93a62dd7fce9d0cc4a73c2d2c19a75
Asal Nesar Noubari <assal.nobari@gmail.com>
2022-10-05 18:07:15 +0200

    legend completed

--- src/views/MapView/components/Legend.js
+++ src/views/MapView/components/Legend.js
@@ -3,15 +3,36 @@ import { useEffect } from "react";
 import "../styles/Map.css";
 function Legend({ map }) {
   console.log(map);
+  const getColor = (d) => {
+    return d > 90
+      ? "#109146"
+      : d > 70
+      ? "#7CBA43"
+      : d > 60
+      ? "#FFCC06"
+      : d > 50
+      ? "#F58E1D"
+      : d >= 0
+      ? "#BF1E24"
+      : "#fff";
+  };
   useEffect(() => {
     if (map) {
-      const legend = L.control({ position: "bottomright" });
+      const legend = L.control({ position: "bottomleft" });
 
       legend.onAdd = () => {
         const div = L.DomUtil.create("div", "info legend");
-        div.innerHTML =
-          "<h4>This is the legend</h4>" +
-          "<b>Lorem ipsum dolor sit amet consectetur adipiscing</b>";
+        const grades = [100, 90, 70, 60, 50];
+        const texts = ["Excellent", "Good", "Fair", "Uncertain", "Poor"];
+        let labels = [];
+
+        for (let i = 0; i < grades.length; i++) {
+          labels.push(
+            '<i style="background:' + getColor(grades[i]) + '"></i> ' + texts[i]
+          );
+        }
+
+        div.innerHTML = labels.join("<br>");
         return div;
       };
 
