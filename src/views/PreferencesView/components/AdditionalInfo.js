70adc4f5f7f3b85c0ee50857a8a7ff20f85d964e
Asal Nesar Noubari <assal.nobari@gmail.com>
2022-10-20 20:51:55 +0200

    added tooltip to filter checkbox

--- src/views/PreferencesView/components/AdditionalInfo.js
+++ src/views/PreferencesView/components/AdditionalInfo.js
@@ -5,15 +5,21 @@ import "../../../App.css";
 const AdditionalInfo = ({ userData, setUserData }) => {
   return (
     <div>
-      <Form>
+      <Form className="filter">
         <Form.Check
+          className="filter"
           type="checkbox"
           id="custom-switch"
           label="Filter out the destinations over the  budget"
           onChange={(e) =>
             setUserData({ ...userData, isPriceImportant: e.target.checked })
           }
-        />
+        ></Form.Check>
+        <span className="tooltip-box">
+          If you select the checkbox the over-budget destinations will be
+          filtered out. if it is not selected, price would have an impact on the
+          recommendations just like any other attribute.
+        </span>
       </Form>
       <hr style={{ marginBottom: "1.2rem" }} />
       <Row>
