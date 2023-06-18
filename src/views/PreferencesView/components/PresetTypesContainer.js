98fc508d65b217da7a4b2cf01ba094284b4fcc58
Cem Nasit Sarica <cemnasitsarica@gmail.com>
2023-06-18 13:16:26 +0200

    Novice mode cards

--- src/views/PreferencesView/components/PresetTypesContainer.js
+++ src/views/PreferencesView/components/PresetTypesContainer.js
@@ -1,50 +1,44 @@
-import React from "react";
-import Attribute from "./Attribute";
+import React, {useMemo} from "react";
 import * as myConstant from "../../../data/constantData";
-import { Card, Button, Row, Col } from "react-bootstrap";
+import { Card, Row, Col } from "react-bootstrap";
 import culinary from "../../../images/culinary.png";
 
 export const PresetTypesContainer = ({ userData, setUserData }) => {
+    console.log(userData.Attributes)
+
+    const presetTypesRows = useMemo(() => {
+        let rows = [];
+        for (let i = 0; i < 3; i++) {
+            let cols = [];
+            for (let j = 0; j < 3; j++) {
+                cols.push(
+                    <Col>
+                        <Card
+                            className="mb-3"
+                            style={{ alignItems: "center", backgroundColor: myConstant.COLORS[i * 3 + j] }}
+                            onClick={() => console.log("Clicked!")}
+                        >
+                            <Card.Img variant="top" src={culinary} style={{ height: "100px", width: "100px" }} />
+                            <Card.Body>
+                                <Card.Text style={{ color: "white" }}>{Object.keys(userData.Attributes)[i * 3 + j]}</Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                );
+            }
+            rows.push(
+                <Row>
+                    {cols}
+                </Row>
+            );
+        }
+        return rows;
+    }, [userData.Attributes]);
+
+
     return (
-        <Row>
-            <Col>
-                <Card className="mb-3">
-                    <Card.Img variant="top" src="holder.js/100px180" />
-                    <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the
-                            bulk of the card's content.
-                        </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
-                </Card>
-                <Card className="mb-3">
-                    <Card.Img variant="top" src="holder.js/100px180" />
-                    <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the
-                            bulk of the card's content.
-                        </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
-                </Card>
-            </Col>
-            <Col>
-                <Card className="mb-3">
-                    <Card.Img variant="top" src={culinary} />
-                    <Card.Body>
-                        <Card.Title style={{ color: "black" }}>Culinary</Card.Title>
-                    </Card.Body>
-                </Card>
-                <Card className="mb-3">
-                    <Card.Img variant="top" src="holder.js/100px180" />
-                    <Card.Body>
-                        <Card.Title style={{ color: "black" }}>Culinary</Card.Title>
-                    </Card.Body>
-                </Card>
-            </Col>
-        </Row>
+        <>
+            {presetTypesRows}
+        </>
     );
 };
--- src/views/PreferencesView/components/TravelMonths.js
+++ src/views/PreferencesView/components/TravelMonths.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo, useCallback } from "react";
 import { ButtonGroup, ToggleButton } from "react-bootstrap";
 import "../../../App.css";
 
@@ -7,39 +7,42 @@ const months = ["January", "February", "March", "April", "May", "June", "July", 
 
 const TravelMonths = ({ userData, setUserData }) => {
 
-  const handleMonthChange = (index) => {
+  const handleMonthChange = useCallback((index) => {
     let newMonths = [...userData.Months];
     newMonths[index] = newMonths[index] === 100 ? 0 : 100;
     setUserData({
       ...userData,
       Months: newMonths,
     });
-  };
+  }, [userData, setUserData]);
 
-  let buttonGroups = [];
-  for (let i = 0; i < 3; i++) {
-    let buttons = [];
-    for (let j = 0; j < 4; j++) {
-      buttons.push(
-        <ToggleButton
-          key={`${i * 4 + j} - ${userData.Months[i * 4 + j]}`}
-          id={`radio-${i * 4 + j}`}
-          type="checkbox"
-          style={{ width: "20%", }}
-          variant="outline-primary"
-          checked={userData.Months[i * 4 + j] === 100}
-          onChange={(e) => handleMonthChange(i * 4 + j)}
-        >
-          <p style={{ fontSize: "0.8rem", margin: 0, color: "white" }}>{months[i * 4 + j]}</p>
-        </ToggleButton>
+  const buttonGroups = useMemo(() => {
+    let buttonGroups = [];
+    for (let i = 0; i < 3; i++) {
+      let buttons = [];
+      for (let j = 0; j < 4; j++) {
+        buttons.push(
+          <ToggleButton
+            key={`${i * 4 + j} - ${userData.Months[i * 4 + j]}`}
+            id={`radio-${i * 4 + j}`}
+            type="checkbox"
+            style={{ width: "20%", }}
+            variant="outline-primary"
+            checked={userData.Months[i * 4 + j] === 100}
+            onChange={(e) => handleMonthChange(i * 4 + j)}
+          >
+            <p style={{ fontSize: "0.8rem", margin: 0, color: "white" }}>{months[i * 4 + j]}</p>
+          </ToggleButton>
+        );
+      }
+      buttonGroups.push(
+        <ButtonGroup size="sm" style={{ width: "95%" }} key={`${i * 4}`}>
+          {buttons}
+        </ButtonGroup>
       );
     }
-    buttonGroups.push(
-      <ButtonGroup size="sm" style={{ width: "95%" }} key={`${i * 4}`}>
-        {buttons}
-      </ButtonGroup>
-    );
-  }
+    return buttonGroups;
+  }, [userData.Months, handleMonthChange]);
 
 
   return (
